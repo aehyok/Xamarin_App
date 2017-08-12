b51947ba4741cff50113e595ff09737dcb2ed091
aehyok <aehyok@vip.qq.com>
2017-08-12 13:41:25 +0800

    添加项目文件。

---
+++ aehyok.DemoApp/aehyok.DemoApp/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace aehyok.DemoApp.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
