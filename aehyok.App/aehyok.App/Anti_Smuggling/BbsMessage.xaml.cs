24a151943c739e207dc2c74fc98383310dd7c097
刘启明 <aehyok@vip.qq.com>
2018-04-21 17:23:28 +0800

    ListView和Grid嵌套方案修改，还需继续调整

--- aehyok.App/aehyok.App/Anti_Smuggling/BbsMessage.xaml.cs
+++ aehyok.App/aehyok.App/Anti_Smuggling/BbsMessage.xaml.cs
@@ -15,9 +15,25 @@ namespace aehyok.App.Anti_Smuggling
 	{
 		public BbsMessage ()
 		{
-            this.BindingContext = new[] { "a", "b", "c" };
+
             InitializeComponent ();
 		}
+
+        protected override void OnAppearing()
+        {
+            var source = new List<ListViewItem>() {
+                new ListViewItem(){ Title="Title1",Detail="Detail1",DateTimes="呈请时间：2018年4月21日"},
+                new ListViewItem(){ Title="Title2",Detail="Detail2",DateTimes="呈请时间：2018年4月21日"},
+                new ListViewItem(){ Title="Title3",Detail="Detail3",DateTimes="呈请时间：2018年4月21日"},
+                new ListViewItem(){ Title="Title4",Detail="Detail4",DateTimes="呈请时间：2018年4月21日"},
+                new ListViewItem(){ Title="Title5",Detail="Detail5",DateTimes="呈请时间：2018年4月21日"},
+                new ListViewItem(){ Title="Title6",Detail="Detail6",DateTimes="呈请时间：2018年4月21日"},
+                new ListViewItem(){ Title="Title7",Detail="Detail7",DateTimes="呈请时间：2018年4月21日"},
+                new ListViewItem(){ Title="Title8",Detail="Detail8",DateTimes="呈请时间：2018年4月21日"},
+            };
+            listView.ItemsSource = source;
+            base.OnAppearing();
+        }
         void OnItemTapped(object sender, ItemTappedEventArgs e)
         {
             if (e == null) return; // has been set to null, do not 'process' tapped event
