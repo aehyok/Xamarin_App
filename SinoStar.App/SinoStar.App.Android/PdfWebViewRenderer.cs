d0ca41ef61b120bd8e5eeb94f3a90a53c8d3c8ab
刘启明 <aehyok@vip.qq.com>
2018-04-25 17:37:42 +0800

    展示本地PDF功能实现

---
+++ SinoStar.App/SinoStar.App.Android/PdfWebViewRenderer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SinoStar.App;
+using SinoStar.App.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(PdfWebView), typeof(PdfWebViewRenderer))]
+namespace SinoStar.App.Droid
+{
+    public class PdfWebViewRenderer : WebViewRenderer
+    {
+        public PdfWebViewRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<WebView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                var customWebView = Element as PdfWebView;
+                Control.Settings.AllowUniversalAccessFromFileURLs = true;
+                Control.LoadUrl(string.Format("file:///android_asset/pdfjs/web/viewer.html?file={0}", string.Format("file:///android_asset/Content/{0}", WebUtility.UrlEncode(customWebView.Uri))));
+            }
+        }
+    }
+}
---
+++ SinoStar.App/SinoStar.App/PdfWebView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SinoStar.App
+{
+    public class PdfWebView : WebView
+    {
+        public static readonly BindableProperty UriProperty = BindableProperty.Create<PdfWebView, string>(p => p.Uri, default(string));
+
+        public string Uri
+        {
+            get { return (string)GetValue(UriProperty); }
+            set { SetValue(UriProperty, value); }
+        }
+    }
+}
