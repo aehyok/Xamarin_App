956caa3df439e3127e67403c028829b8a0b2f6a7
刘启明 <aehyok@vip.qq.com>
2018-05-12 17:54:00 +0800

    ListView布局调整变更

--- SinoStar.App/SinoStar.App/Anti_Smuggling/BbsMessage.xaml.cs
+++ SinoStar.App/SinoStar.App/Anti_Smuggling/BbsMessage.xaml.cs
@@ -20,13 +20,14 @@ namespace SinoStar.App.Anti_Smuggling
 
         protected override void OnAppearing()
         {
+            base.OnAppearing();
             var source = new List<ListViewItem>() {
-                new ListViewItem(){ Title="Title1Title1Title1Title1",Detail="Detail1Detail1Detail1Detail1Detail1Detail1Detail1",DateTimes="呈请时间：2018年4月21日"},
-                new ListViewItem(){ Title="Title1Title1Title1Title222",Detail="Detail2",DateTimes="呈请时间：2018年4月21日"},
-                new ListViewItem(){ Title="Title1Title1Title1Titl3333333",Detail="Detail3",DateTimes="呈请时间：2018年4月21日"},
-                new ListViewItem(){ Title="Title1Title1Title144444444444",Detail="Detail4",DateTimes="呈请时间：2018年4月21日"},
-                new ListViewItem(){ Title="Title1Title1Titl5555",Detail="Detail5",DateTimes="呈请时间：2018年4月21日"},
-                new ListViewItem(){ Title="Title6",Detail="Detail6",DateTimes="呈请时间：2018年4月21日"},
+                new ListViewItem(){ Title="1",Detail="1",DateTimes="呈请时间：2018年4月21日"},
+                new ListViewItem(){ Title="2",Detail="Detail2",DateTimes="呈请时间：2018年4月21日"},
+                new ListViewItem(){ Title="3",Detail="Detail3",DateTimes="呈请时间：2018年4月21日"},
+                new ListViewItem(){ Title="4",Detail="Detail4",DateTimes="呈请时间：2018年4月21日"},
+                new ListViewItem(){ Title="5",Detail="Detail5",DateTimes="呈请时间：2018年4月21日"},
+                new ListViewItem(){ Title="6",Detail="Detail6",DateTimes="呈请时间：2018年4月21日"},
                 new ListViewItem(){ Title="Title7",Detail="Detail7",DateTimes="呈请时间：2018年4月21日"},
                 new ListViewItem(){ Title="Title8",Detail="Detail8",DateTimes="呈请时间：2018年4月21日"},
                 new ListViewItem(){ Title="Title1Title1Titl5555",Detail="Detail5",DateTimes="呈请时间：2018年4月21日"},
@@ -35,14 +36,12 @@ namespace SinoStar.App.Anti_Smuggling
                 new ListViewItem(){ Title="Title8",Detail="Detail8",DateTimes="呈请时间：2018年4月21日"}
             };
             listView.ItemsSource = source;
-            base.OnAppearing();
         }
         void OnItemTapped(object sender, ItemTappedEventArgs e)
         {
-            if (e == null) return; // has been set to null, do not 'process' tapped event
-            Debug.WriteLine("Tapped: " + e.Item);
-            DisplayAlert("提示页面", e.Item.ToString(), "OK ");
-            ((ListView)sender).SelectedItem = null; // de-select the row
+            //if (e == null) return;
+            //Debug.WriteLine("Tapped: " + e.Item);
+            //DisplayAlert("提示页面", e.Item.ToString(), "OK ");
         }
 
         private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
